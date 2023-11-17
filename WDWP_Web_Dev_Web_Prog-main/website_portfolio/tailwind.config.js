5b4abe5f6e9ce3c45d4ea5cc82f26feb41767236
Newbie092 <febrianto142004@gmail.com>
2023-11-17 22:59:28 +0700

    First Commit

---
+++ WDWP_Web_Dev_Web_Prog-main/website_portfolio/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      letterSpacing: {
+        ngempit:'-0.5em',
+        ngangkang: '.5em',
+      }
+    },
+  },
+  plugins: [],
+}
+
